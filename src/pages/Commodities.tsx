6a480bc92c13e7e01523d5d269d875cd1802bc74
abhiraichura <raichuraabhishek3@gmail.com>
2026-06-13 22:33:02 +0530

    Update Commodities.tsx

--- src/pages/Commodities.tsx
+++ src/pages/Commodities.tsx
@@ -1,1 +1,10 @@
-import { Helmet } from 'react-helmet-async'; const Commodities = () => <div className="py-24"><h1 className="text-6xl text-center">Commodities We Handle</h1></div>; export default Commodities;
+const Commodities = () => (
+  <div className="py-24">
+    <div className="max-w-7xl mx-auto px-6">
+      <h1 className="text-6xl font-bold text-center mb-12">Commodities We Handle</h1>
+      {/* Full content from master prompt will go here */}
+    </div>
+  </div>
+);
+
+export default Commodities;
